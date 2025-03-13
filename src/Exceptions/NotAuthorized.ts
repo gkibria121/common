93bbdf66e2a80af1278e7b2a6921b0eea657fa7a
gkibria121 <gkibria121@gmail.com>
2025-03-14 05:08:03 +0600

    update

---
+++ src/Exceptions/NotAuthorized.ts
@@ -0,0 +1,15 @@
+import { CustomError } from "../contracts/CustomErrorContract";
+import { Errors } from "../types/error";
+class NotAuthorized extends CustomError {
+  constructor() {
+    super("User is Unauthorized!");
+  }
+  statusCode: number = 401;
+  serializeErrors(): Errors {
+    return {
+      message: "User is Unauthorized!",
+    };
+  }
+}
+
+export default NotAuthorized;
