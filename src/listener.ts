7429874c43c3f96e1b930f161c707e2f1ccf6a2c
gkibria121 <gkibria121@gmail.com>
2025-03-17 11:17:57 +0600

    update

--- src/listener.ts
+++ src/listener.ts
@@ -1,6 +1,6 @@
 import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
-import TicketCreatedListener from "./events/ticket-created-listener";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 const id = randomBytes(10).toString("hex");
 const stan = nats.connect("nats-streaming", id, {
   url: "http://localhost:4222",
