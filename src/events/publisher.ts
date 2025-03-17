4221dac6c4e5e4594e2f594bfe3bb97c64c5e1b0
gkibria121 <gkibria121@gmail.com>
2025-03-17 11:49:31 +0600

    update

--- src/events/publisher.ts
+++ src/events/publisher.ts
@@ -2,9 +2,9 @@ import { Stan } from "node-nats-streaming";
 import { Event } from "./event";
 
 abstract class Publisher<T extends Event> {
-  abstract subject: Event["subject"];
+  abstract subject: T["subject"];
   constructor(private client: Stan) {}
-  publish(data: Event["data"]): Promise<void> {
+  publish(data: T["data"]): Promise<void> {
     return new Promise((resolve, reject) => {
       this.client.publish(this.subject, JSON.stringify(data), (error) => {
         if (error) {
