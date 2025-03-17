2990a46df9cff195ba4371fd3ab7bd4511518e5a
gkibria121 <gkibria121@gmail.com>
2025-03-17 11:17:37 +0600

    update

--- src/publisher.ts
+++ src/publisher.ts
@@ -1,6 +1,6 @@
 import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
-import TicketCreatedPublisher from "./events/ticket-created-publisher";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 import { title } from "process";
 const id = randomBytes(10).toString("hex");
 const stan = nats.connect("nats-streaming", id, {
