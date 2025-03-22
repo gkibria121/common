3d6a10b6bc6237ccfb08c2c6c5ec6061aa9083b8
gkibria121 <gkibria121@gmail.com>
2025-03-22 09:12:09 +0600

    add subjects

--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -2,6 +2,8 @@ enum Subject {
   TicketCreated = "ticket:created",
   TicketUpdated = "ticket:updated",
   TicketDeleted = "ticket:deleted",
+  OrderCreated = "order:created",
+  OrderCancelled = "order:cancelled",
 }
 
 export { Subject };
