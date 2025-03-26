9d0ca262b924df353a98deecaa2065061016421a
gkibria121 <gkibria121@gmail.com>
2025-03-26 11:58:46 +0600

    remove callback from publish method

--- src/events/publisher.ts
+++ src/events/publisher.ts
@@ -4,13 +4,12 @@ import { Event } from "./event";
 abstract class Publisher<T extends Event> {
   abstract subject: T["subject"];
   constructor(private client: Stan) {}
-  publish(data: T["data"], callback: () => void): Promise<void> {
+  publish(data: T["data"]): Promise<void> {
     return new Promise((resolve, reject) => {
       this.client.publish(this.subject, JSON.stringify(data), (error) => {
         if (error) {
           reject(error);
         }
-        callback();
         resolve();
       });
     });
